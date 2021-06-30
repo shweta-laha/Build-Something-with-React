f650d899e8cf40b342583c410e548daa7aafdc7b
shweta laha <shwetalaha5@gmail.com>
2021-06-30 20:25:10 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+   
+
+<p style={{ fontSize:"60px"}}><b style={{color:'red'}}>M </b><b style={{color:'blue'}}>L</b> <b style={{color:'yellow'}}>H</b></p>
+<b><p style={{color:"rgba(20, 8, 56)", fontSize:40}}> MLH INIT DAY: 3 CHALLENGE</p></b>
+<mark style={{backgroundColor:"black"}}>  <p style={{fontFamily:" 'Patrick Hand SC', cursive", fontSize:30, color:"white"}}><b>Build Something with React</b></p></mark>
+
+
+
+        <p style={{ fontFamily: "Quicksand", textAlign:"center", color:"white" , fontSize:20}}><b>
+        Hey there, I'm Shweta Laha, a passionate and enthusiastic girl with some web designing skill . I'm looking for opportunities to upskill my journey in this field. I'm quite creative and hard working. I love to make some creative handmade stuffs and paintings. I'm really enjoying participating in MLH INIT 2021. Today is Day 3 and I'm making this project for the challenge "Build Something with React". Hope you all like it.
+      </b>  </p>
+        <p
           className="App-link"
-          href="https://reactjs.org"
+    
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+     <b style={{fontSize:"30px"}}>Happy Hacking!</b>
+
+<br/><br/>
+
+     <a href="https://www.linkedin.com/in/shweta-laha-9254741b2/" alt="h"><img src="https://img.icons8.com/ios-glyphs/60/000000/linkedin.png" alt="h"/></a>
+
+<a href="https://github.com/shweta-laha"><img src="https://img.icons8.com/material-outlined/48/000000/github.png" alt="hi"/></a>
+
+<a href="https://twitter.com/02Tithi"><img src="https://img.icons8.com/ios-glyphs/48/000000/twitter.png" alt="tw"/></a>
+        </p>
       </header>
     </div>
   );
